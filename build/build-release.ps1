76fe26c3450532fa54b7295c07200c2deee46080
Matthew King <mking@mking.net>
2023-10-21 09:46:04 +0800

    Update build script.

--- build/build-release.ps1
+++ build/build-release.ps1
@@ -3,7 +3,8 @@ $output = "$root/artifacts"
 $projects = @(
     "$root/src/KingMigrations/KingMigrations.csproj",
     "$root/src/KingMigrations.Sqlite/KingMigrations.Sqlite.csproj",
-    "$root/src/KingMigrations.SqlServer/KingMigrations.SqlServer.csproj"
+    "$root/src/KingMigrations.SqlServer/KingMigrations.SqlServer.csproj",
+    "$root/src/KingMigrations.PostgreSql/KingMigrations.PostgreSql.csproj"
 )
 
 foreach ($project in $projects) {
